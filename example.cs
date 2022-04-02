d3b0ff5472be2d1c22d347f7aaefe25b9f006ede
Brad Marder <bradmarder@users.noreply.github.com>
2022-04-01 22:35:16 -0400

    Simplify with SimplifyStatementRemove

--- example.cs
+++ example.cs
@@ -691,7 +691,6 @@ public class Program
         ushort var2 = default(ushort);
         long var3 = default(long);
         byte var8 = default(byte);
-        long[][] var15 = default(long[][]);
         short[] var21 = default(short[]);
         return ref s_46[0];
     }
