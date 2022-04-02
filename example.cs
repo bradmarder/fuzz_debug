1b73bd9e1a9ce0ec2c7ce73cb4adc84c1053c42b
Brad Marder <bradmarder@users.noreply.github.com>
2022-04-01 22:35:04 -0400

    Simplify with SimplifyStatementRemove

--- example.cs
+++ example.cs
@@ -726,7 +726,6 @@ public class Program
         long[][] var15 = default(long[][]);
         ushort var16 = default(ushort);
         bool var17 = default(bool);
-        ushort var19 = default(ushort);
         short[] var21 = default(short[]);
         S1 var22 = default(S1);
         return ref s_46[0];
