6200c91a73b18d42df855c9ed622af9e92072137
Brad Marder <bradmarder@users.noreply.github.com>
2022-04-01 22:36:29 -0400

    Simplify with RemoveTypeDeclaration

--- example.cs
+++ example.cs
@@ -5,19 +5,6 @@ public class C0
     }
 }
 
-public struct S1
-{
-    public short F0;
-    public long F1;
-    public short F2;
-    public S1(short f0, long f1, short f2): this()
-    {
-        F0 = f0;
-        F1 = f1;
-        F2 = f2;
-    }
-}
-
 public class Program
 {
     public static void Main()
