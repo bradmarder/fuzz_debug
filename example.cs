7335c5ca6a4a13c37f77f82823703790c9909381
Brad Marder <bradmarder@users.noreply.github.com>
2022-04-01 22:35:45 -0400

    Simplify with SimplifyInitializer

--- example.cs
+++ example.cs
@@ -482,7 +482,7 @@ public class Program
     public static uint s_97 = 1637596950U;
     public static ulong s_98 = 0UL;
     public static short[] s_99 = new short[]{1, 0, 1};
-    public static ushort[][] s_100 = new ushort[][]{new ushort[]{65534}, new ushort[]{0, 2, 44207, 31181, 57937}, new ushort[]{44758, 0}};
+    public static ushort[][] s_100 = new ushort[][]{new ushort[]{65534}, new ushort[]{0, 2, 44207, 31181, 57937}, new ushort[]{44758}};
     public static S0[, ] s_101 = new S0[, ]{{new S0(10UL, -106, 1, 127, 1L, 10, 0L, 15357392491071761068UL, -9223372036854775807L)}};
     public static bool s_102 = true;
     public static ulong s_103 = 15780061626666752982UL;
