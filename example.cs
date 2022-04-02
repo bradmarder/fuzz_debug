a83691de64b73def209acc0f5e580462cc2df015
Brad Marder <bradmarder@users.noreply.github.com>
2022-04-01 22:35:55 -0400

    Simplify with SimplifyStructAddConstructorInitializers

--- example.cs
+++ example.cs
@@ -172,7 +172,7 @@ public struct S1
     public short F0;
     public long F1;
     public short F2;
-    public S1(short f0, long f1, short f2)
+    public S1(short f0, long f1, short f2): this()
     {
         F0 = f0;
         F1 = f1;
