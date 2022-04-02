169c9aea3c89b005a651824d0713223c4d315061
Brad Marder <bradmarder@users.noreply.github.com>
2022-04-01 22:36:08 -0400

    Simplify with RemoveMethodDeclaration

--- example.cs
+++ example.cs
@@ -300,11 +300,6 @@ public class Program
         return default(S0);
     }
 
-    public static ushort M11(bool arg0, bool[][] arg1, ushort[] arg2, uint arg3)
-    {
-        return default(ushort);
-    }
-
     public static ushort[] M17()
     {
         return default(ushort[]);
