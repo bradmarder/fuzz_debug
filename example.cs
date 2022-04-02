d526a238b82770a689b1be9a0b5e1d08f6f51309
Brad Marder <bradmarder@users.noreply.github.com>
2022-04-01 22:36:27 -0400

    Simplify with SimplifyField

--- example.cs
+++ example.cs
@@ -68,7 +68,6 @@ public class Program
 {
     public static short[] s_8 = new short[]{27419};
     public static byte s_10 = 169;
-    public static uint s_12 = 10U;
     public static C1 s_121 = new C1(32766, 1, 0);
     public static void Main()
     {
