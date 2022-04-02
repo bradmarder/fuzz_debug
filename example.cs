cc2132968a318bcb38d4fe9d35519cc4ce339e5e
Brad Marder <bradmarder@users.noreply.github.com>
2022-04-01 22:36:20 -0400

    Simplify with SimplifyField

--- example.cs
+++ example.cs
@@ -134,7 +134,6 @@ public class Program
     public static byte s_82 = 48;
     public static C0 s_84 = new C0(0, 0, false);
     public static long s_91 = -9223372036854775808L;
-    public static byte[] s_95 = new byte[]{0};
     public static byte s_96 = 0;
     public static sbyte s_104 = 119;
     public static uint s_107 = 10U;
