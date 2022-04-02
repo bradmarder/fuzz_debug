46a4bf4223a4994fbe16f01e4ded8f064d2a0983
Brad Marder <bradmarder@users.noreply.github.com>
2022-04-01 22:35:08 -0400

    Simplify with SimplifyStatementRemove

--- example.cs
+++ example.cs
@@ -684,7 +684,6 @@ public class Program
 
     public static ref C0 M67(S1 arg0, ushort arg1, C0[][] arg2)
     {
-        I0 var1 = default(I0);
         int var2 = default(int);
         C1 var3 = default(C1);
         I0[] var6 = default(I0[]);
